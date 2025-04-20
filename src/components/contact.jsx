5530022ce6347402d25ed241f8c260de679d30a4
Erick Fontes <erickfontes2024@gmail.com>
2025-04-19 21:29:08 -0300

    corrige nome do arquivo ContactForm

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ContactForm from './ContactForm';
+import ContactForm from './contactform';
 
 const Contact = () => (
   <section id="contact" className="section">
